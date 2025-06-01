18701338828ff567575b9e88a7acbc48871472ea
Gaurav Kushwaha <147244300+Gauravkushwa@users.noreply.github.com>
2025-06-01 15:35:33 +0530

    Add files via upload

---
+++ ES6 syntext error and Type Connection.js
@@ -0,0 +1,23 @@
+const checkout = {
+    items: [],
+    total: 0,
+  
+    addItem(item) {
+      const price = Number(item.price);
+      if (typeof price !== 'number' || isNaN(price)) {
+        console.log("Invalid price.");
+        return;
+      }
+      this.items.push(item);
+      this.total += price;
+    },
+  
+    getTotal() {
+      return `Total: $${this.total.toFixed(2)}`;
+    }
+  }
+  
+  checkout.addItem({ name: "Coffee Maker", price: "99.95" });
+  checkout.addItem({ name: "Milk", price: 3.50 });
+  console.log(checkout.getTotal());
+  
